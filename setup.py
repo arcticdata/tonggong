038ccff9df50786412583c5bc07117370cb49570
ZHOU Cheng <c.zhou@live.com>
2019-11-05 15:49:31 +0800

    add Pipfile

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open('README.md', 'r', encoding='utf-8') as f:
     description = f.read()
@@ -10,17 +10,18 @@ setup(
     description='',
     long_description=description,
     long_description_content_type='text/markdown',
-    author='ArcticData',
+    author='Arctic Data',
     author_email='hello@datarc.cn',
     url='https://github.com/arcticdata/tonggong',
     license='MIT License',
-    # install_requires='',
-    # packages=find_packages(exclude=['tests']),
+    install_requires='',
+    packages=find_packages(exclude=['tests']),
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
