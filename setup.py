7a1e1f7db281e68fee7a1de89c2e88ff1750b3de
ZHOU Cheng <c.zhou@live.com>
2019-11-14 19:01:44 +0800

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='tonggong',
-    version='0.0.3',
+    version='0.0.4',
     description='',
     long_description=description,
     long_description_content_type='text/markdown',
