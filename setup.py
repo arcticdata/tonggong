0f6a41ced8f418b87e758ed5a3273e4005917834
ZHOU Cheng <c.zhou@live.com>
2019-11-20 16:17:36 +0800

    docs: add description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     name='tonggong',
     version='0.0.4',
-    description='',
+    description='Universal toolkit',
     long_description=description,
     long_description_content_type='text/markdown',
     author='Arctic Data',
