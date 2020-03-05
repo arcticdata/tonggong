74fbc9f74783e34c16848ae428b3baebe6bbe3b8
ZHOU Cheng <c.zhou@live.com>
2020-03-05 14:28:33 +0800

    fix unittest

--- tests/test_constant.py
+++ tests/test_constant.py
@@ -6,5 +6,5 @@ from tonggong.constant import DatabaseAdapter
 class BunchTestCase(unittest.TestCase):
     def test_choices(self):
         for name, value in DatabaseAdapter.choices():
-            self.assertTrue(isinstance(name, str))
+            self.assertTrue(isinstance(name, int))
             self.assertTrue(isinstance(value, str))
