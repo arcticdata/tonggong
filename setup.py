8dacfd4bd72321c80a271d4298c21bc30074eac6
ZHOU Cheng <c.zhou@live.com>
2023-02-21 23:36:07 +0800

    Bump version: 0.2.18 → 0.2.19

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="tonggong",
-    version="0.2.18",
+    version="0.2.19",
     description="Universal toolkit",
     long_description=description,
     long_description_content_type="text/markdown",
